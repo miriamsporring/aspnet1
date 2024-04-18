494a6b7777ec7d5e48ff6d38166525e6a44205a1
Miriam Sporring <mirian.sporring@gmail.com>
2024-04-18 16:22:39 +0200

    första giten

---
+++ Silicon_WebApi/Infrastructure/Migrations/20240410074629_Courses Table Fixed.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CoursesTableFixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "likesInNumbers",
+                table: "Courses",
+                newName: "LikesInNumbers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LikesInNumbers",
+                table: "Courses",
+                newName: "likesInNumbers");
+        }
+    }
+}
---
+++ Silicon_WebApp/WebApp/Controllers/DefaultController.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers;
+
+public class DefaultController(HttpClient httpClient) : Controller
+{
+    private readonly HttpClient _httpClient = httpClient;
+
+	public IActionResult Home()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Subscribe(SubscribeViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+			var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("https://localhost:7291/api/subscribe", content);
+		    if (response.IsSuccessStatusCode)
+            {
+                TempData["StatusMessage"] = "You are now subscribed";
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Conflict)
+            {
+                TempData["StatusMessage"] = "You are already subscribed";
+            }
+        }
+        else
+        {
+			TempData["StatusMessage"] = "Invalid email address";
+		}
+
+        return RedirectToAction("Home", "Default", "subscribe");
+
+    }
+}
